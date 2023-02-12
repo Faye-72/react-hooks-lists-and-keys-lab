0e1c61cfb45f37d8653b34619e9068d7ec65f79d
fmitei <faith.mitei@trustgro.com>
2023-02-12 12:53:46 +0300

    completed test

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,14 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  const navigations = links.map((link, index) => {
+    return (
+      <a key={index} href={`#${link}`}>
+        {link}
+      </a>
+    );
+  });
+  return <nav>{navigations}</nav>;
 }
 
 export default NavBar;
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -2,11 +2,21 @@ import React from "react";
 import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
-  console.log(projects);
+  console.log("projects prop here .....", projects);
+  const projectObject = projects.map((item, index) => {
+    return (
+      <ProjectItem
+        key={index}
+        name={item.name}
+        about={item.about}
+        technologies={item.technologies}
+      />
+    );
+  });
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">{projectObject}</div>
     </div>
   );
 }
